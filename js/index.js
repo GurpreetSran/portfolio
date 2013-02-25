b9a37cb265d5aa683843c42a81021f66a3c3b814
Gurpreet Singh <gurizox@yahoo.com>
2013-02-25 22:53:10 +0000

    Added content & styling

--- js/index.js
+++ js/index.js
@@ -1,69 +1,133 @@
 var projects = [
     {
-        url: 'assets/images/sun-main.jpg',
-        description: 'Project Description',
-        skills: 'html, as3'  
+        project_url: 'http://www.thesun.co.uk/sol/homepage/news/4445859/A-brand-new-look-for-The-Sun-website.html',           
+        image_url: 'assets/images/sun-main.jpg',
+        description: "<p>The Sun has the tenth-largest circulation of any newspaper in the world and the largest circulation"+
+        			  " of any daily newspaper in the United Kingdom. It had an average daily circulation of 2,409,811 copies" +
+        			  " in January 2013. Between January and June 2012 the paper had an average daily readership of approximately" +
+        			  " 7.3 million.</p>"+
+        			  "<p>It was an exciting project to lead. We rewrote entire tired & outdated front end for SUN online and gave it a brand-new look. " +
+                     "Along with many new features. We updated the site to utilise the power of HTML 5 which performed 3 " +
+                     "times better than the old site.<p>",
+        skills: 'Modular JavaScript, jQuery, HTML5, CSS, Flash Builder, Fluid Grid System, Escenic'  
     },
     {
-        url: 'assets/images/match-center-main.jpg',
+    	project_url: 'http://www.thesun.co.uk/sol/homepage/sport/football/4539172/Sun-Football-gets-bigger-and-better.html',
+        image_url: 'assets/images/match-center-main.jpg',
         description: 'Project 2',
-        skills: ''
+        skills: 'Modular JavaScript, jQuery, Escenic'
     },
     {
-        url: 'assets/images/ipad-poc-main.jpg',
+        project_url: 'http://gurpreetsran.github.com/ipad-poc/',
+        image_url: 'assets/images/ipad-poc-main.jpg',
         description: 'Project Description',
-        skills: 'html, as3'  
+        skills: 'HTML5, CSS3, jQuery, touch-punch'  
     },
     {
-        url: 'assets/images/notw-main.jpg',
-        description: 'Project 2',
-        skills: ''
+        project_url: 'http://www.newsoftheworld.co.uk/',
+        image_url: 'assets/images/notw-main.jpg',
+        description: '',
+        skills: 'JavaScript, jQuery, CSS, AS3, Escenic'
     },
     {
-        url: 'assets/images/thetimes-main.jpg',
+        project_url: 'http://www.thetimes.co.uk/tto/news/',
+        image_url: 'assets/images/thetimes-main.jpg',
         description: 'Project Description',
-        skills: 'html, as3'  
+        skills: 'JavaScript, jQuery, CSS, Escenic'  
     },
     {
-        url: 'assets/images/thesundaytimesmain.jpg',
+        project_url: 'http://www.thesundaytimes.co.uk/sto/',
+        image_url: 'assets/images/thesundaytimesmain.jpg',
         description: 'Project 2',
-        skills: ''
+        skills: 'JavaScript, jQuery, CSS, Escenic'
     },
     {
-        url: 'assets/images/',
+        project_url: 'https://itunes.apple.com/us/app/anna-richardsons-body-blitz/id408864573?mt=8',
+        image_url: 'assets/images/anna-main.jpg',
         description: "<p><a href= '#'>Anna's Richardson's</a> Body Blitz Diet is a two week diet plan that guarantees up to 7lb weight loss in just 14 days." + 
         			  "Featuring the 5 RULES to guarantee weight loss, over 35 delicious, easy-to-prepare recipes, and the exclusive PANIC" + 
         			  "BUTTON to keep dieters on the straight and getting narrower!</p> <p>Whilst this app has been developed specifically"+
         			  "for iphone use, ELIPS studio is a plug-in for Flex that allows us to create native applications for multiple mobile" +
         			  "platforms (Apple, Android, Windows Mobile and Symbian). Therefore, because the app was built in ELIPS it is possible" +
         			  "to repurpose the app at a later date for use on another platform, should this be required.",
-        skills: 'html, as3'  
+        skills: 'Flash Builder, Action Script 3, MXML, Elips'  
     },
     {
-        url: 'assets/images/',
-        description: 'Project 2',
-        skills: ''
+        project_url: '',
+        image_url: 'assets/images/atoz-main.jpg',
+        description: "<p> Philip Allan Updates is part of the <a target='blank' href='http://www.hoddereducation.co.uk/'>Hodder Education Group</a>, "+ 
+        			  "a division of <a target='_blank' href='http://www.hachettelivre.co.uk/'>Hachette UK</a>. We have been working " +
+        			  "on Hodder Education Digital solutions for number of years. Over this period of time, we have successfully" +
+        			  "developed number of web tools for them.</p>" +
+        			  "<p>This is an AIR based AZ Admin tool developed for <a href='http://www.philipallan.co.uk' target='_blank'>Philip" +
+        			  "Alan</a>. This tool stiches together html, CSV  and other files in the xml format. This XML/XHTML will eventually" +
+        			  "be used by .net  based backend system. I’ve attached the sample files and project brief if you want to have a" +
+        			  "quick look. I’ve individually done this project. Our designer gave me a hand in  skinning &amp; basic layout." +
+        			  "Please use links below to install and download required files for testing.</p>",
+        skills: 'Abode AIR, AS3, MXML'
     },
     {
-        url: 'assets/images/',
+        project_url: 'http://www.gsquare.co.uk/',
+        image_url: 'assets/images/greensquare-main.jpg',
+        description: "<p>Green Square is a corporate finance and advisory practice. They provide insightful and balanced advice to the shareholders of independent media and marketing communications businesses. They help them prepare their businesses for sale and ultimately achieve the optimum transaction.</p><p>Apart from having neat and attractive design, use of jQuery makes user experience even better. Implementing web 2.0 features such as google maps, twitter and rss feed was fun!</p>",
+        skills: 'JavaScript, jQuery, Kentico CMS, XHTML, CSS'  
+    },
+    {
+        project_url: 'http://www.accenture.com/us-en/pages/index.aspx',
+        image_url: 'assets/images/accenture-main.jpg',
+        description: "<p>Accenture’s expertise and resources cover the full M&A process, from developing a strategy, to screening M&A" +
+        			  "targets, to performing due diligence and integrating acquired entities. The M&A strategies we help develop benefit" +
+        			  "from proven frameworks and roadmaps that clarify the value to be gained from M&A as well as the most effective" +
+        			  "ways to attain it.<p/>" +
+        			  "<p>We use MOSS (Microsoft Office SharePoint Server) extensively to develop microsites for Accenture." +
+        			  "It's first integrated server platform that aims to provide web content management, enterprise content" +
+        			  "services, and enterprise search, as well as shared business processes and business intelligence dash" +
+        			  "boarding to the small/medium enterprise. This effective ability to control content comes at the cost of" +
+        			  "rigidness in terms of features. Good knowledge of JavaScript / jQuery and AS3, allows me to customise" +
+        			  "(hack, up to some extent) templates and fulfil branding (and very demanding) requirements of Accenture.</p>",
+        skills: 'XHTML, MOSS, CSS, jQuery, JavaScript, Flash, AS3'
+    },
+    {
+        project_url: 'http://fabulousmag.co.uk/',
+        image_url: 'assets/images/fab-main.jpg',
         description: 'Project Description',
-        skills: 'html, as3'  
+        skills: 'HTML, Flash Player, CSS, Escenic'  
     },
     {
-        url: 'assets/images/',
-        description: 'Project 2',
-        skills: ''
+        project_url: 'http://www.hoddereducation.co.uk/Home.aspx',
+        image_url: 'assets/images/hodder-main.jpg',
+        description: "<p>As one of the UK's largest educational publishers, Hodder Education provides trusted textbooks and" + 
+        			  "sophisticated digital resources to a number of diverse users groups. The objective for this website was" +
+        			  "to upgrade the Hodder Education website to a new platform that is better supported the needs of a growing" +
+        			  "publisher delivering a steady flow of new product to market.</p>" +
+        			  "<p>This website is based on the Kentico Web Content Management System." +
+        			  "We have been working with Hodder for number of years now. Over these years, " +
+        			  "I have been fixing CSS and JavaScript based bugs on this site.",
+        skills: 'JavaScript, XHTML, CSS'
     },
     {
-        url: 'assets/images/',
-        description: 'Project Description',
-        skills: 'html, as3'  
+        project_url: 'http://map.prg.aero/',
+        image_url: 'assets/images/prague-main.jpg',
+        description: "<p>Prague Airport is the operator of the most important international airport in the Czech Republic" +
+        			  "and the biggest airport among the new EU member states. There are also 5 regular cargo carriers and" +
+        			  "other tens of companies providing charter flights from there.</p>" +
+        			  "<p>This interactive map application allows to browse the Terminals or locate a particular shop or restaurant"+
+        			  "in either Terminal 1 or 2. Search, zooming and printing are some nifty features of this xml driven AS3" +
+        			  "flash piece.</p>",
+        skills: 'Flash, XML'
     },
     {
-        url: 'assets/images/',
-        description: 'Project 2',
-        skills: ''
-    }
+        project_url: 'http://development.mmtdigital.co.uk/otto_credentials/Otto_Flash_Credentials.html',
+        image_url: 'assets/images/otto-main.jpg',
+        description: "<p><a href='http://www.ottogroup.com/home.html?&amp;L=0' target='_blank'>Otto Group</a> are inspired and" +
+        			  "united by our shared values, which transcend national borders. Otto see ourselves as enthusiastic partners" +
+        			  "to our customers, as creative strategists within an integrated network and as focused managers" +
+        			  "making decisions with long-lasting effects. </p>" +
+        			  "<p>This piece of creative flash has been developed for Otto and It was sponsored by Accenture." +
+        			  "This flash piece dynamically gets  data from XML. You can easily customise it by modifying some" +
+        			  "predefined settings in XML such as intro clip, type of template etc.</p>",
+        skills: 'Flash, XML'
+    }    
 ];
 
 $(document).ready(function() {
@@ -74,11 +138,7 @@ $(document).ready(function() {
 		
 		$('#about-more').slideToggle();
 		
-		
-		var txt = $moreBtn.text() == 'Skills & Services' ? 'Skills & Services ^' : 'Skills & Services';
-		
-		$moreBtn.text(txt);			
-		 
+		$moreBtn.toggleClass("active");
 		
 	});	
 	
@@ -124,7 +184,12 @@ $(document).ready(function() {
         var current_id = $(this).attr("id");
         var current_header = $(this).find("h3").html();    		
   		
-		var str = "<div style='text-align: center'><img style='width: 100%;' src='"+projects[current_id].url+"'/></div><h1> " + current_header +"</h1>" +
+       if(projects[current_id].project_url != "") {
+          current_header = "<a href='"+projects[current_id].project_url+"' target='blank'>" +current_header+ "</a>";
+       }   		
+  		
+  		
+		var str = "<div style='text-align: center'><img style='width: 100%;' src='"+projects[current_id].image_url+"'/></div><h1> " + current_header +"</h1>" +
 		projects[current_id].description+ "<p><span>Skills Used: "+projects[current_id].skills+"</p>"; 
         
 		$lightBox.find('#content').html(str);        
@@ -139,4 +204,3 @@ $(document).ready(function() {
     } 	
 	
 });
-
