203e3bf4282c2e4a4051a0d5b0bf576d547c9b75
Gurpreet Singh <gurizox@yahoo.com>
2013-02-25 00:29:49 +0000

    Added Content & did some styling

--- js/index.js
+++ js/index.js
@@ -31,7 +31,12 @@ var projects = [
     },
     {
         url: 'assets/images/',
-        description: 'Project Description',
+        description: "<p><a href= '#'>Anna's Richardson's</a> Body Blitz Diet is a two week diet plan that guarantees up to 7lb weight loss in just 14 days." + 
+        			  "Featuring the 5 RULES to guarantee weight loss, over 35 delicious, easy-to-prepare recipes, and the exclusive PANIC" + 
+        			  "BUTTON to keep dieters on the straight and getting narrower!</p> <p>Whilst this app has been developed specifically"+
+        			  "for iphone use, ELIPS studio is a plug-in for Flex that allows us to create native applications for multiple mobile" +
+        			  "platforms (Apple, Android, Windows Mobile and Symbian). Therefore, because the app was built in ELIPS it is possible" +
+        			  "to repurpose the app at a later date for use on another platform, should this be required.",
         skills: 'html, as3'  
     },
     {
@@ -70,7 +75,7 @@ $(document).ready(function() {
 		$('#about-more').slideToggle();
 		
 		
-		var txt = $moreBtn.text() == 'More' ? 'Less ^' : 'More';
+		var txt = $moreBtn.text() == 'Skills & Services' ? 'Skills & Services ^' : 'Skills & Services';
 		
 		$moreBtn.text(txt);			
 		 
@@ -120,7 +125,7 @@ $(document).ready(function() {
         var current_header = $(this).find("h3").html();    		
   		
 		var str = "<div style='text-align: center'><img style='width: 100%;' src='"+projects[current_id].url+"'/></div><h1> " + current_header +"</h1>" +
-  				   "<br><p>" +projects[current_id].description+"</p> <br> <p>"+projects[current_id].skills+"</p>"; 
+		projects[current_id].description+ "<p><span>Skills Used: "+projects[current_id].skills+"</p>"; 
         
 		$lightBox.find('#content').html(str);        
        
